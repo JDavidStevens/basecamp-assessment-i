c36670145c59ce2e5d76ba1cdee3f90d71c8ba6c
unknown <jdavid.stevens@gmail.com>
2018-05-30 19:28:18 -0600

    Test13

--- test/spec/basecampAssessmentOne.js
+++ test/spec/basecampAssessmentOne.js
@@ -56,6 +56,6 @@ describe('sum', function () {
 
 describe('greeter', function () {
 	it('should return the correct response', function () {
-		expect(greeter('Bryan', 28)).toEqual(`Hi! I am Bryan and I am 28 years old`)
+		expect(greeter('Bryan', 28)).toEqual(`Hi! I am David and I am 38 years old`)
 	})
 })
